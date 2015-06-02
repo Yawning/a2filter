0176be28470d7ff4025faeb8823ef31ff27105ed
Yawning Angel <yawning@schwanenlied.me>
2015-06-02 14:46:56 +0000

    Fix licence blurb, no functional changes.

--- a2filter.go
+++ a2filter.go
@@ -1,7 +1,7 @@
 // a2filter.go - A2 bloom filter
 //
 // To the extent possible under law, the Yawning Angel waived all copyright
-// and related or neighboring rights to or-ctl-filter, using the creative
+// and related or neighboring rights to a2filter, using the creative
 // commons "cc0" public domain dedication. See LICENSE or
 // <http://creativecommons.org/publicdomain/zero/1.0/> for full details.
 
